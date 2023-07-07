e97044214504a8943ade7c99d79e26c50941626e
Joaco Esteban <dev.joaco.esteban@gmail.com>
2023-07-08 00:06:49 +0200

    change

--- src/app/app.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home () {
-  return (
-    <main>
-      <iframe src="https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf" frameborder="0"></iframe>
-    </main>
-  )
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home () {
+  return (
+    <main>
+      <iframe src="https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf"></iframe>
+    </main>
+  )
+}
