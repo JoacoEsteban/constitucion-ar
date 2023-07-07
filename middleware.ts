3f1db3dcd970da53f4da87161496908c600b4267
Joaco Esteban <dev.joaco.esteban@gmail.com>
2023-07-07 23:46:43 +0200

    refactor to next

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,10 @@
-export const middleware = (req, res, next) => {
-  res.writeHead(301, { Location: 'https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf' })
-  res.end()
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export const config = {
+  matcher: '/',
+}
+
+export default function middleware () {
+  NextResponse.redirect('https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf')
 }
