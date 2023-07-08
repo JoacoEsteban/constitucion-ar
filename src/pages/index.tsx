6265bbfb1ef95b88993e60de5ad22e8c1deed080
Joaco Esteban <dev.joaco.esteban@gmail.com>
2023-07-08 12:04:54 +0200

    iframe size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home () {
   return (
     <main>
-      <iframe src="https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf"></iframe>
+      <iframe width={100} height={100} src="https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf"></iframe>
     </main>
   )
 }
