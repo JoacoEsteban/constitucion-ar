fe1f861c620d9e39959b3769992a311430efd228
Joaco Esteban <dev.joaco.esteban@gmail.com>
2023-07-08 12:18:07 +0200

    * fix(descargar.tsx): replace window.location.href with getServerSideProps to redirect to the PDF file

--- src/pages/descargar.tsx
+++ src/pages/descargar.tsx
@@ -1,4 +1,12 @@
-export default function Home () {
-  // redirect to the pdf
-  window.location.href = 'https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf'
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: 'https://www.argentina.gob.ar/sites/default/files/constitucion_0.pdf',
+      permanent: true,
+    }
+  }
 }
+
+export default function Download () {}
